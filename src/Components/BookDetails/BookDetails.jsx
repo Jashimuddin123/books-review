3bfd86f3a413038256061bef5b683e349d73dfa8
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-05-23 22:48:16 +0600

    adding book details page

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -11,22 +11,31 @@ const BookDetails = () => {
     const { bookName, author, image, category, rating, description } = book;
 
     return (
-        <div className="book-details">
-            <div className="card w-80 bg-base-100 shadow-xl mx-auto">
-                <figure>
-                    <img className="w-56 rounded h-56" src={image} alt="book" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{bookName}</h2>
-                    <p>By: {author}</p>
-                    <div>{category}</div>
-                    <div className="flex items-center gap-4">
-                        <span>{rating}</span>
-                    </div>
-                    <p>{description}</p>
-                </div>
-            </div>
-        </div>
+
+    <div>
+      <div>
+        <img src={book.image} alt="" />
+      </div>
+      <div>
+       
+      </div>
+    </div>
+        // <div className="book-details">
+        //     <div className="card w-80 bg-base-100 shadow-xl mx-auto">
+        //         <figure>
+        //             <img className="w-56 rounded h-56" src={image} alt="book" />
+        //         </figure>
+        //         <div className="card-body">
+        //             <h2 className="card-title">{bookName}</h2>
+        //             <p>By: {author}</p>
+        //             <div>{category}</div>
+        //             <div className="flex items-center gap-4">
+        //                 <span>{rating}</span>
+        //             </div>
+        //             <p>{description}</p>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
